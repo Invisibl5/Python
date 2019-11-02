b889f6bac3c03fb80a75333228bfdb109b59c83b
First <adhithyaguhan@gmail.com>
2019-11-02 15:24:53 -0400

    Homework Chapter 3

---
+++ Python Book Work & Homework/Chapter 3.py
@@ -0,0 +1,10 @@
+games=["gaming", "eating", "cleeping"]
+foods=["ice cream", "pizza", "Naan"]
+favorites=games+foods
+print(favorites)
+
+print(3*25+2*40)
+
+firstname="Adhithya"
+lastname="Guhan"
+print("Hi there,%s" %firstname+" "+lastname)
