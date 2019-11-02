1fe2c55ff082fafbed4e026ce2bef3d46c513d91
First <adhithyaguhan@gmail.com>
2019-11-02 15:28:54 -0400

    Homework Chapter 5

---
+++ Python Book Work & Homework/Chapter 5.py
@@ -0,0 +1,22 @@
+#1 Are You Rich? answer: prints "I'm rich!!"
+
+#2 Twinkies!
+twinkies=600
+if twinkies<100:
+    print("too few twinkies")
+elif twinkies > 500:
+    print("too many twinkies")
+#3 Just The Right Number
+money=400
+if 100<money<500:
+    print("money is between 100 and 500")
+elif 1000<money<5000:
+    print("money is between 1000 and 5000")
+#4 I Can Fight Those Ninjas
+ninjas=7
+if ninjas>50:
+    print("That's too many")
+elif 10<ninjas<30:
+    print("It'll be a struggle, but I can take 'em")
+elif ninjas<10:
+    print("I can fight those ninjas!")
